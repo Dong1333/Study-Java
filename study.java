9ee048cbb3ed2d21c001368f832b17cc6c640a5a
동근 <93319756+Dong1333@users.noreply.github.com>
2021-11-09 22:47:51 +0900

    Add files via upload

--- study.java
+++ study.java
@@ -162,8 +162,10 @@ public class study{
 		}
 		scanner.close();
 	}
-}*/
-
+}
+//2차원 평면에서 직사각형은 왼쪽 상단 모서리와 오른쪽 하단 모서리의 두점으로 표현한다 (100,100)과 (200,200)의
+//두점으로 이루어진 사각형이 있을때, (x,y)값을 입력받고 직사각형 안에 있는지 판별하라.
+//예) 150, 200     있습니다,   50, 70   없습니다.
 import java.util.Scanner;
 public class study{
 	public static void main(String args[]){
@@ -171,10 +173,11 @@ public class study{
 		
 		System.out.print("점 (x,y)의 좌표를 입력하시오 >>> ");
 		
+		//x값, y값 받기  
 		int x = scanner.nextInt();
 		int y = scanner.nextInt();
 
-		
+		//조건 : 200 >= (x, y) >= 100.  x, y모두 200보다 작고 100보다 크다를 표현한다. 
 		if( x >= 100 && x <= 200 && y >= 100 && x <= 200) {
 			System.out.printf("(%d, %d)는 사각형 안에 있습니다.",x ,y);
 		}
@@ -183,4 +186,38 @@ public class study{
 		}
 		scanner.close();
 	}
-}
+}*/
+
+import java.util.Scanner;
+public class study{
+	public static void main(String args[]){
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("연산 >>> ");
+		
+		//x값, y값 받기  
+		int x = scanner.nextInt();
+		String sign = scanner.next();
+		int y = scanner.nextInt();
+
+
+			if(sign.equals("+") == true) {
+			System.out.printf(x + sign + y + "의 계산 결과는 %d", x + y );
+			}
+			else if(sign.equals("-") == true) {
+				System.out.printf(x + sign + y + "의 계산 결과는 %d", x - y );
+			}
+			else if(sign.equals("*") == true) {
+				System.out.printf(x + sign + y + "의 계산 결과는 %d", x * y );
+			}
+			else if(sign.equals("/") == true) {
+				if(x == 0 || y == 0) {
+					System.out.printf("0으로 나눌 수 없습니다.");
+				}
+				else
+				System.out.printf(x + sign + y + "의 계산 결과는 %d", x / y );
+				}
+			}					
+	}
+
+
