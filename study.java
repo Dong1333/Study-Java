557d6487efa1b969eba4e6bd266190b493d63271
동근 <93319756+Dong1333@users.noreply.github.com>
2021-11-16 23:09:22 +0900

    Add files via upload

--- study.java
+++ study.java
@@ -561,4 +561,24 @@ public class study{
 }
 */
 
+import java.util.Scanner;
+public class study{
+	public static void main(String args[]) {
+		
+	Scanner scanner = new Scanner(System.in);
+	
+	System.out.print("소문자 알파벳 하나를 입력하시오  >>>  ");
+	String alphabet = scanner.next();
+	char a = alphabet.charAt(0);
+	char b = alphabet.charAt(0);
+	for(int i = 97; a >= 97;  ){
+		for(int j = 97; j <= b; j++) {
+			System.out.print((char)j);
+		}
+		 System.out.println(" ");
+		 a--;
+		 b = a;
+	}
+	}
+}
 
