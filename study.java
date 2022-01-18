e73fd1142bccc0ea0b79cc3823733f729d8f6840
동근 <93319756+Dong1333@users.noreply.github.com>
2022-01-18 22:48:58 +0900

    Add files via upload

--- study.java
+++ study.java
@@ -1396,10 +1396,54 @@ public class study{
 		ColorPoint cp = new ColorPoint(5, 6, "blue");
 		cp.showColorPoint();
 	}
+
+
+interface MouseDriver{
+	final int BUTTONS = 3;
+	int VERSION;
+	void move();
+	public int click();
+	protected int out();
+	static public void drag();
+	default void drop() { System.out.println("drop");}
+}
+
 */
 
-interface Moi
-	
+
+class Pen { // 모든 펜의 공통 속성
+	private int amount; // 남은 량
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+}
+
+class SharpPencil extends Pen { // 샤프펜슬
+	private int width; // 펜의 굵기
+}
+
+class Ballpen extends Pen { // 볼펜
+	private String color; // 볼펜의 색
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+}
+
+class FountainPen extends Ballpen { // 만년필
+	public void refill(int n) {
+		setAmount(n);
+	}
+}
 
 
 
