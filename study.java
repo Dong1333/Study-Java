beb78eed0679434e98134acc7c4432bc6fc421c7
동근 <93319756+Dong1333@users.noreply.github.com>
2022-02-15 20:46:21 +0900

    Add files via upload

--- study.java
+++ study.java
@@ -2066,7 +2066,100 @@ public class study {
 		System.out.println("아바타는 " + index + "번째 요소입니다.");
 	}
 }
+public class study {
+	public static void main(String[] args) {
+		int max = 0;
+		int[] array = { 1, 5, 3, 8, 2 };
+
+		for(int i = 0 ; i<array.length; i++) {
+			if(max < array[i])
+			   max = array[i];
+		}
+
+		System.out.println("max : " + max);
+	}
+}
+
+public class study {
+	public static void main(String[] args) {
+		int[][] array = { 
+				{ 95, 86 }, 
+				{ 83, 92, 96 }, 
+				{ 78, 83, 93, 87, 88 } 
+				};
+
+		int sum = 0;
+		double avg = 0.0;
+
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				sum += array[i][j];
+				avg = (double) sum / 10;
+
+			}
+		}
+		System.out.println("sum : " + sum);
+		System.out.println("avg : " + avg);
+	}
+}
 */
+
+// 다음은 키보드로부터 학생 수와 각 학생들의 점수를 입력받아서, 최고 점수 및 평균 점수를 구하는 프로그램입니다.
+// 실행결과를 보고 알맞게 작성해보세요.
+import java.util.Scanner;
+
+public class study {
+	public static void main(String[] args) {
+		boolean run = true;
+		int studentNum = 0;
+		int[] scores = null;
+		Scanner scanner = new Scanner(System.in);
+
+		while (run) {
+			System.out.println("----------------------------------------------");
+			System.out.println("1.학생수 | 2.점수입력 | 3.점수리스트 | 4.분석 | 5.종료");
+			System.out.println("----------------------------------------------");
+			System.out.print("선택 > ");
+
+			int selectNo = Integer.parseInt(scanner.nextLine());
+
+			if (selectNo == 1) {
+			  System.out.println("학생수 > ");
+			  studentNum =Integer.parseInt(scanner.nextLine());
+			  scores = new int[studentNum];
+			} else if (selectNo == 2) {
+				for(int i = 0; i < scores.length; i++) {
+					System.out.println("scores["+i+"] > ");
+					scores[i] = Integer.parseInt(scanner.nextLine());
+				}
+			} else if (selectNo == 3) {
+				for(int i = 0; i < scores.length; i++) {
+					System.out.println("scores["+i+"] > " + scores[i]);
+				}
+			} else if (selectNo == 4) {
+				int max = 0;
+				int sum = 0;
+				double avg = 0.0;
+						
+				for(int i = 0; i < scores.length; i++) {
+					sum += scores[i];
+					if(max < scores[i]) {
+						max = scores[i];
+					}
+					avg = (double) sum / scores.length;
+				}
+				System.out.println("최고 점수 : " + max);
+				System.out.println("평균 점수 : " + avg);
+			} else if (selectNo == 5) {
+				run = false;
+			}
+		}
+
+		System.out.println("프로그램 종료");
+	}
+}
+
+
 
 
 
