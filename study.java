96e73602f86fbf7a40f3da02f38e2fcf1abb17bf
동근 <93319756+Dong1333@users.noreply.github.com>
2022-02-17 21:26:14 +0900

    Add files via upload

--- study.java
+++ study.java
@@ -2102,7 +2102,7 @@ public class study {
 		System.out.println("avg : " + avg);
 	}
 }
-*/
+
 
 // 다음은 키보드로부터 학생 수와 각 학생들의 점수를 입력받아서, 최고 점수 및 평균 점수를 구하는 프로그램입니다.
 // 실행결과를 보고 알맞게 작성해보세요.
@@ -2159,6 +2159,7 @@ public class study {
 		System.out.println("프로그램 종료");
 	}
 }
+*/
 
 
 
